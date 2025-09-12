662586bacabac994d68ea0176926659bba4ec4e4
Boris Ushakov <inlvert@gmail.com>
2025-09-12 10:21:36 +0300

    add DB_CONNECTION

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        uri: configService.get<string>('DB_CONNECTION'),
+        autoLoadModels: true,
+        synchronize: true,
+        logging: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
