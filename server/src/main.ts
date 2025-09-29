c4a812d88a1f23b8c793404c06e6d13650993c96
Boris Ushakov <inlvert@gmail.com>
2025-09-29 12:33:07 +0300

    add cors settings

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://personal-recipe-discovery-platform-two.vercel.app',
+      'http://localhost:3000', // для розробки
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
