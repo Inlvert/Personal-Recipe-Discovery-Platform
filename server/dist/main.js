39c0ec97a3f57353ab9a855614a3d8283c442323
Boris Ushakov <inlvert@gmail.com>
2025-09-29 12:58:45 +0300

    add new build

--- server/dist/main.js
+++ server/dist/main.js
@@ -5,10 +5,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: [
-            'https://personal-recipe-discovery-platform-two.vercel.app',
-            'http://localhost:3000',
-        ],
+        origin: 'https://personal-recipe-discovery-platform-two.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
