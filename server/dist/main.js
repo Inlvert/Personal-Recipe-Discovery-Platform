bc08bef1cfac9e75533a481f6c5fe7347f0f0393
Boris Ushakov <inlvert@gmail.com>
2025-09-29 12:37:35 +0300

    add server build

--- server/dist/main.js
+++ server/dist/main.js
@@ -4,7 +4,15 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: [
+            'https://personal-recipe-discovery-platform-two.vercel.app',
+            'http://localhost:3000',
+        ],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    });
     await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
